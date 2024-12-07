6340e3601a463bc2a35c3334dd1a4d03b974fac4
Pablo da Silva Bezerra <Pdsbezerra1316@gmail.com>
2024-12-07 14:19:34 -0300

    organizing project

---
+++ js/main.js
@@ -0,0 +1,27 @@
+import { lists } from "./data.js"
+
+// async(fetch('data.json').then((response=>{
+//     response.json().then((dados)=>{
+//         lists = Array.from(dados)
+//     })
+// })))
+
+
+// lists.forEach((el)=>{
+//     console.log(el)})
+
+// Construindo o cardápio
+
+lists.forEach((el)=>{
+    console.log(el)
+    const card = document.createElement('div')
+})
+
+
+/*<div class="dessert">
+  <div class="image"></div>
+  <div class="adCart">Add. Cart</div>
+  <p class="type">type</p>
+  <h3>Name</h3>
+  <p class="price">$0,00</p>
+ </div> */
